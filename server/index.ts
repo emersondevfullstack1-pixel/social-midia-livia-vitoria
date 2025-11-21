b4c7b191f9f2ba869b68e3870211680cb0c9d2bb
Emerson Silva <emersondevfullstack1@gmail.com>
2025-11-21 12:15:49 -0300

    feat: site completed

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,6 @@ import { createServer } from "http";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// O uso de import.meta.url e fileURLToPath é necessário para obter o __dirname em módulos ES.
 var _filename = fileURLToPath(import.meta.url);
 var _dirname = path.dirname(_filename);
 
@@ -12,10 +11,6 @@ async function startServer() {
   const app = express();
   const server = createServer(app);
 
-  // **CORREÇÃO APLICADA AQUI:**
-  // Em produção (Vercel), o arquivo index.js está em `dist/`.
-  // O frontend está em `dist/public/`.
-  // O caminho deve ser ajustado para apontar para a pasta `public` dentro de `dist`.
   const staticPath =
     process.env.NODE_ENV === "production"
       ? path.resolve(_dirname, "public") // Se o _dirname for `dist/`, "public" aponta para `dist/public`
@@ -25,11 +20,6 @@ async function startServer() {
 
   app.get("*", (_req, res) => {
     res.sendFile(path.join(staticPath, "index.html"));
-  });
-
-  const port = process.env.PORT || 3e3;
-  server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}/`);
   });
 }
 
